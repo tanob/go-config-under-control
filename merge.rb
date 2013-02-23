426da5388cf799aeb96dbbc8aa346be442634e36
Your Name <you@example.com>
2013-02-23 21:31:56 +0100

    using nokogiri to change the config file.

--- merge.rb
+++ merge.rb
@@ -1,6 +1,15 @@
 #!/bin/env ruby
 
 require 'bundler'
+Bundler.require
 
-`cp cruise-config.xml /etc/go/`
+config_file = '/etc/go/cruise-config.xml'
+
+config = Nokogiri::XML(File.open(config_file))
+new_config = Nokogiri::XML(File.open('cruise-config.xml'))
+
+(config/:pipelines).remove
+config.at('cruise') << new_config/:pipelines
+
+File.open(config_file, 'w') {|f| f.write(config) }
 
