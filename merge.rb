1795a2ac77055080b9c673dc23499e39ef6e92a2
Your Name <you@example.com>
2013-02-23 21:41:54 +0100

    Config schema validates order of elements.

--- merge.rb
+++ merge.rb
@@ -9,7 +9,7 @@ config = Nokogiri::XML(File.open(config_file))
 new_config = Nokogiri::XML(File.open('cruise-config.xml'))
 
 (config/:pipelines).remove
-config.at('cruise') << new_config/:pipelines
+config.at('cruise/server').after new_config/:pipelines
 
 File.open(config_file, 'w') {|f| f.write(config) }
 
